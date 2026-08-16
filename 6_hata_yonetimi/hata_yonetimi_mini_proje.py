9c14b3b6a35955138eade44c317cc3e393bc857a
Ceren Altunbas <crnaltunbas35@gmail.com>
2026-08-16 11:31:39 +0300

    Hata yönetimi ile ilgili mini proje yapıldı

---
+++ 6_hata_yonetimi/hata_yonetimi_mini_proje.py
@@ -0,0 +1,54 @@
+"""
+Bozuk veri temizleme
+veri:
+        70
+        85
+        abc
+        90
+        50
+        hata
+        60
+Amaç:
+    - dosyayı oku
+    - sayıya çevrilemeyen satıları atla
+    - geçerli notları topla
+    - ortalama hesapla
+"""
+
+### bu benim düşündüğüm yol 
+# notlar=[70, 85, "abc", 90, 50, "hata", 60]
+
+# try:
+#     with
+#     for i in notlar:
+#         toplam = 0
+#         y =0 #eleman sayısı
+#         if i is int:
+#          toplam = toplam + i
+#          y +=1
+#          print(toplam/y)
+#         elif i is str:
+#            break
+# finally:
+ # çözümmm
+
+notlar = [] # notları saklamak için
+hata_sayisi = 0 # hata sayisini tespit etmek için
+
+with open("notlar.txt", "r", encoding="utf-8") as dosya:
+
+    for satir in dosya:
+
+        try: 
+            not_degeri = int(satir.strip()) # satir eşit değildir int ise burada hata olacak
+            notlar.append(not_degeri)
+        except ValueError:
+            print(f"Hatalı veri bulundu: {satir.strip()}")
+            hata_sayisi += 1 # dosyada hatalı satir sayısı
+
+print(f"notlar: {notlar}") # [70, 85, 90, 50, 60]
+print(f"hata_sayisi: {hata_sayisi}") #  2
+
+ortalama = sum(notlar) / len(notlar)
+
+print(f"ortalama: {ortalama}")
